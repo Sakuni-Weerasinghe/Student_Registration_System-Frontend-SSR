44c044a6d64d519a38f6a8874ed018efce25ae31
Sakuniw <ayonaweerasinghe@gmail.com>
2023-12-16 19:54:42 +0530

    Create Footer Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/core/header/header.component";
+import { FooterComponent } from "./components/core/footer/footer.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, HeaderComponent]
+    imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'Student_Registration_System-Frontend';
