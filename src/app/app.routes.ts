f4753dde210e6ed951e482f9bb97369e5770db30
Sakuniw <ayonaweerasinghe@gmail.com>
2023-12-17 22:30:47 +0530

    Add router Like for course registration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { LoginComponent } from './components/login/login.component';
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
 import { authGuard } from './guards/auth.guard';
 import { StudentRegistrationComponent } from './components/student-registration/student-registration.component';
+import { CourseRegistrationComponent } from './components/course-registration/course-registration.component';
 
 export const routes: Routes = [
     {path:'',component:HomeComponent},
     {path: 'login', component:LoginComponent},
     {path:'dashboard',component:AdminDashboardComponent,canActivate:[authGuard]},
-    {path:'student-registration',component:StudentRegistrationComponent}
+    {path:'student-registration',component:StudentRegistrationComponent},
+    {path:'course-registration',component:CourseRegistrationComponent}
 ];
