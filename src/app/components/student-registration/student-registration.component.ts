5710f789415c0be7dffdf64396fbd47bdf8d8b23
Sakuniw <ayonaweerasinghe@gmail.com>
2023-12-17 20:10:39 +0530

    Change student register form to template driven form

--- src/app/components/student-registration/student-registration.component.ts
+++ src/app/components/student-registration/student-registration.component.ts
@@ -1,46 +1,32 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormBuilder, FormGroup, FormControl,Validators } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
+import { Student } from '../../models/studet.model';
 
 @Component({
   selector: 'app-student-registration',
   standalone: true,
-  imports: [ReactiveFormsModule,CommonModule],
+  imports: [FormsModule,CommonModule],
   templateUrl: './student-registration.component.html',
   styleUrl: './student-registration.component.css'
 })
 export class StudentRegistrationComponent {
-  studentRegistrationForm! : FormGroup;
-
-  constructor(private fb: FormBuilder){}
-
-  ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
-    this.studentRegistrationForm = this.fb.group({
-      firstname: ['',Validators.required],
-      lastname: ['',Validators.required],
-      birthday: ['',Validators.required],
-      phone: ['',Validators.required],
-      email: ['',Validators.required],
-      address: ['',Validators.required]
-    })
-  }
+  studentRegisterRequest: Student = {
+    studentId: " ",
+    firstname: " ",
+    lastname: " ",
+    birthday: new Date(),
+    gender: " ",
+    email: " ",
+    phone: " ",
+    addressline1: " ",
+    addressline2: " ",
+    addressline3: " "
+  };
 
   registerStudent() {
-
-  }
-
-  private validateAllFormFields(formGroup: FormGroup){
-    Object.keys(formGroup.controls).forEach(field => {
-      const control = formGroup.get(field);
-      if(control instanceof FormControl){
-        control.markAsDirty({onlySelf:true})
-      }else if(control instanceof FormGroup){
-        this.validateAllFormFields(control)
-      }
-    })
+    console.log(this.studentRegisterRequest);
   }
 
 
---
+++ src/app/models/studet.model.ts
@@ -0,0 +1,13 @@
+export interface Student{
+    studentId: string;
+    firstname: string;
+    lastname: string;
+    birthday: Date;
+    gender: string;
+    email: string;
+    phone: string;
+    addressline1: string;
+    addressline2: string;
+    addressline3: string;
+    
+}
