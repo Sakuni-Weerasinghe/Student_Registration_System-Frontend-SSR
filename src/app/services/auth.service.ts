1d65acdefafdd7ae542ad9d4f0fac9896cf70732
Sakuniw <ayonaweerasinghe@gmail.com>
2023-12-17 10:14:00 +0530

    Create auth service logout method

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -7,10 +8,17 @@ import { Injectable } from '@angular/core';
 export class AuthService {
   private baseUrl:string = "https://localhost:7270/api/Admin/"
 
-  constructor(private http: HttpClient) {  }
+  constructor(
+    private http: HttpClient,
+    private router: Router) {  }
 
   login(loginObj:any){
     return this.http.post<any>(`${this.baseUrl}authenticate`,loginObj)
+  }
+
+  logout(){
+    localStorage.clear();
+    this.router.navigate(['/login']);
   }
 
   storeToken(tokenValue: string){
