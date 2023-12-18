4d3fdbd404bdf3f93684d1bb4560873fb0f211f2
Sakuniw <ayonaweerasinghe@gmail.com>
2023-12-18 09:43:19 +0530

    Change Student Model Attributes Names

--- src/app/models/studet.model.ts
+++ src/app/models/studet.model.ts
@@ -1,13 +1,13 @@
 export interface Student{
     studentId: number;
-    firstname: string;
-    lastname: string;
+    firstName: string;
+    lastName: string;
     birthday: Date;
     gender: string;
     email: string;
-    phone: string;
-    addressline1: string;
-    addressline2: string;
-    addressline3: string;
+    phones: string;
+    addressLine1: string;
+    addressLine2: string;
+    addressLine3: string;
     
 }
