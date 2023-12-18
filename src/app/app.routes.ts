5b8008e69def0a731e0ff237f6d2195f0bb9e687
Sakuniw <ayonaweerasinghe@gmail.com>
2023-12-18 08:37:29 +0530

    Create student enrollment component and route to the admin dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,11 +5,13 @@ import { AdminDashboardComponent } from './components/admin-dashboard/admin-dash
 import { authGuard } from './guards/auth.guard';
 import { StudentRegistrationComponent } from './components/student-registration/student-registration.component';
 import { CourseRegistrationComponent } from './components/course-registration/course-registration.component';
+import { StudentEnrollmentComponent } from './components/student-enrollment/student-enrollment.component';
 
 export const routes: Routes = [
     {path:'',component:HomeComponent},
     {path: 'login', component:LoginComponent},
     {path:'dashboard',component:AdminDashboardComponent,canActivate:[authGuard]},
     {path:'student-registration',component:StudentRegistrationComponent},
-    {path:'course-registration',component:CourseRegistrationComponent}
+    {path:'course-registration',component:CourseRegistrationComponent},
+    {path:'student-enrollment',component:StudentEnrollmentComponent}
 ];
---
+++ src/app/components/student-enrollment/student-enrollment.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-enrollment',
+  standalone: true,
+  imports: [],
+  templateUrl: './student-enrollment.component.html',
+  styleUrl: './student-enrollment.component.css'
+})
+export class StudentEnrollmentComponent {
+
+}
